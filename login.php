cce0a957c91b9558ee6034877899c8c401588836
AdaliCode <fiezayyan@gmail.com>
2024-04-08 09:16:45 +0700

    mengubah isi file login better

--- login.php
+++ login.php
@@ -1,47 +1,61 @@
 <?php
+
+require 'functions.php';
 // cek apakah tombol submit udh dipencet
-if (isset($_POST["submit"])) {
-    if ($_POST["username"] == "admin" && $_POST["password"] == "123") {
-        header("Location: admin.php");
-        exit;
-    } else {
-        $error = true;
+if (isset($_POST["login"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // cek adakah username di table users
+    $result = mysqli_query($db, "SELECT * FROM users WHERE username = '$username' ");
+
+    // mysqli_num_rows ngecek berapa baris yang dikembalikan dari query
+    if (mysqli_num_rows($result) === 1) {
+        // cek password
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($password, $row["password"])) { // password_verify ngecek sama apa gak dengan di db, kebalikan password_hash
+            header("Location: index.php");
+            exit;
+        }
     }
+    $error = true;
 }
+
 ?>
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>Login Admin</h1>
-    <?php if (isset($error)) : ?>
-        <p style="color: red; font-style: italic;">Username atau Pass Salah</p>
-    <?php endif; ?>
-    <ul>
-        <form action="admin.php" method="post">
-            <li>
-                <!-- label dan input bisa berhubungan dengan menggunakan label for dan input id -->
-                <!-- jangan lupa kunjungi snippet -->
-                <label for="username">Username :</label>
-                <input type="text" name="username" id="username">
-            </li>
-            <li>
-                <!-- label dan input bisa berhubungan dengan menggunakan label for dan input id -->
-                <!-- jangan lupa kunjungi snippet -->
-                <label for="password">password :</label>
-                <input type="password" name="password" id="password">
-            </li>
-            <li>
-                <button type="submit" name="submit">Login</button>
-            </li>
-        </form>
-    </ul>
+    <div class="container">
+        <div class="row mt-3 mb-3">
+            <h1>Login User</h1>
+            <div class="col-8">
+                <?php if (isset($error)) : ?>
+                    <p style="color: red; font-style: italic;">Username atau Pass Salah</p>
+                <?php endif; ?>
+                <form action="" method="post">
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" class="form-control border border-dark" name="username" id="username">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control border border-dark" name="password" id="password">
+                    </div>
+                    <button type="submit" name="login" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
