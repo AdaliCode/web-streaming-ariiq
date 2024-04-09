b575aa2fd656f2a2f1df101ba2320dd6fe3e59b2
AdaliCode <fiezayyan@gmail.com>
2024-04-09 10:22:18 +0700

    menerapkan oop constructor dan datype

--- oop.php
+++ oop.php
@@ -1,31 +1,40 @@
 <?php
-
-// Jualan produk
 // class
 class Vidstream
 {
     // property
-    public $title = "title", $vid_type = "video type",
-        $episodes = "0";
+    public $title, $vid_type, $cast, $episodes;
+    // variabel lokal
+    public function __construct($title = "title", $vid_type = "vid_type", $cast = "cast", $episodes = "0")
+    {
+        $this->title = $title;
+        $this->cast = $cast;
+        $this->vid_type = $vid_type;
+        $this->episodes = $episodes;
+    }
     // method
-    public function getLabel()
+    public function getCast()
     {
         // this buat memanggil global atau dalam class
-        return "$this->title, $this->vid_type";
+        return "$this->cast";
+    }
+}
+
+// class yang mengambil data dari class atasan
+// cetak class
+class CetakInfoVidstream
+{
+    // instance dari class Vidstream
+    public function cetak(Vidstream $vstream)
+    {
+        $str = "Jdul : {$vstream->title} | Tipe :  {$vstream->vid_type} |  Pemeran : {$vstream->getCast()}";
+        return $str;
     }
 }
 
 // object
-$vids = new Vidstream();
-$vids->title = "Running Man";
-$vids->vid_type = "Variety Show Korea";
-$vids->episodes = "628";
-$vids2 = new Vidstream();
-$vids2->title = "Pyramid Game";
-$vids2->vid_type = "Drama Korea";
-$vids2->episodes = "10";
+$vids = new Vidstream("Running Man", "Variety Show Korea", "Song Jihyo, Yoo Jae Suk", "628");
+$vids2 = new Vidstream("Pyramid Game", "Drama Korea", "Bona WJSN, Kim Da Ah", "10");
 
-// var_dump($vids);
-echo "<br>Variety Show Korea : " . $vids->getLabel();
-echo "<br>";
-echo "Drama Korea  : " . $vids->getLabel();
+$infovids = new CetakInfoVidstream();
+echo $infovids->cetak($vids2);
