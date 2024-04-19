3bba6416e6a8265027011d1a7835f848ff0b7894
AdaliCode <fiezayyan@gmail.com>
2024-04-19 09:07:01 +0700

    menerapkan mvc controller

--- app/core/App.php
+++ app/core/App.php
@@ -2,20 +2,51 @@
 
 class App
 {
+    // parameter default
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
     public function __construct()
     {
-        // http://localhost/phpmvc/public/index.php?url=as
         $url = $this->parseURL();
-        var_dump($url);
+
+        // kalau ada file Controller di dalam folder controllers
+        if (isset($url[0])) { ///app/controllers/home-> url [0] => home
+            if (file_exists('../app/controllers/' . $url[0] . '.php')) {
+                $this->controller = $url[0];
+                unset($url[0]); //home nya hilang dan diambil controllernya
+            }
+        }
+
+        // ambil controller
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        //method
+        if (isset($url[1])) { ///app/controllers/home/index-> url [1] => index
+            if (method_exists($this->controller, $url[1])) {
+                $this->method = $url[1];
+                unset($url[1]); //method nya hilang dan diambil controllernya
+            }
+        }
+
+        // params
+        if (!empty($url)) {
+            $this->params = array_values($url);
+        }
+
+        // jalankan cntrl * method, serta kirimkan params jika ada
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     public function parseURL()
     {
-        // localhost/phpmvc/public/home/index/
-        if (isset($_GET['url'])) { // home/index/
-            $url = rtrim($_GET['url'], '/'); // menghilangkan last char => home/index
-            $url = filter_var($url, FILTER_SANITIZE_URL); // menghilangkan url aneh 
-            $url = explode('/', $url); // array(2) { [0]=> string(4) "home" [1]=> string(5) "index" }
+        if (isset($_GET['url'])) {
+            # code...
+            $url = rtrim($_GET['url'], '/'); //menghilangkan last char 
+            $url = filter_var($url, FILTER_SANITIZE_URL); //menghilangkan url aneh 
+            $url = explode('/', $url);
             return $url;
         }
     }
