e068aae9b056e13db7543ab2c7b3f99c532e6285
AdaliCode <fiezayyan@gmail.com>
2024-04-19 08:43:31 +0700

    menerapkan mvc routing

--- app/core/App.php
+++ app/core/App.php
@@ -4,6 +4,19 @@ class App
 {
     public function __construct()
     {
-        echo "cuaks!";
+        // http://localhost/phpmvc/public/index.php?url=as
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        // localhost/phpmvc/public/home/index/
+        if (isset($_GET['url'])) { // home/index/
+            $url = rtrim($_GET['url'], '/'); // menghilangkan last char => home/index
+            $url = filter_var($url, FILTER_SANITIZE_URL); // menghilangkan url aneh 
+            $url = explode('/', $url); // array(2) { [0]=> string(4) "home" [1]=> string(5) "index" }
+            return $url;
+        }
     }
 }
